e47ba4aa9857aa029e73f0ac59e675a10f322799
daddyaezzy <pulumbu11@gmail.com>
2024-10-15 22:52:59 +0100

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+  // Other custom configurations can go here
+  reactStrictMode: true, // Enables React strict mode
+  swcMinify: true, // Enables SWC minification for better build performance
+};
 
 export default nextConfig;
