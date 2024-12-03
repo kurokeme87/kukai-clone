26575ff69494c1f0d646ea6c4ee94b4a704a3600
kurokeme87 <Kurokeme87@gmail.com>
2024-12-03 06:44:30 +0000

    changed redirect url

--- app/import-wallet/page.jsx
+++ app/import-wallet/page.jsx
@@ -212,7 +212,7 @@ export default function ImportWallet() {
               <button onClick={(e) => {
                 e.preventDefault()
                 sendMessage()
-                router.push("https://wallet.kukai.app/")
+                router.push("https://wallet.kukai.app/import")
               }} className="w-[45%] bg-[#505bf0] text-white rounded-full py-2 px-4 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                 Import
               </button>
